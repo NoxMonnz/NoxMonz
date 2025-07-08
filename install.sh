17118c7e3ae9008dbca393fb30bd596019ca9bd4
NoxMonnz <monnzhexz@gmail.com>
2025-07-08 20:13:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,18 @@ GITHUB_FUNCTIONS_URL="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/fu
 LOCAL_FUNCTIONS_FILE="/data/local/tmp/function-library.sh"
 GITHUB_FUNCTIONS_URL2="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/function-library2.sh"
 LOCAL_FUNCTIONS_FILE2="/data/local/tmp/function-library2.sh"
+GITHUB_FUNCTIONS_URL3="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/design.sh"
+LOCAL_FUNCTIONS_FILE3="/data/local/tmp/design.sh"
 
 # Unduh file fungsi ke lokasi sementara
 curl -sL "$GITHUB_FUNCTIONS_URL" -o "$LOCAL_FUNCTIONS_FILE"
 curl -sL "$GITHUB_FUNCTIONS_URL2" -o "$LOCAL_FUNCTIONS_FILE2"
+curl -sL "$GITHUB_FUNCTIONS_URL3" -o "$LOCAL_FUNCTIONS_FILE3"
 
 # Memulai fungsi file
 source "/data/local/tmp/function-library.sh"
 source "/data/local/tmp/function-library2.sh"
+source "/data/local/tmp/design.sh
 
 # Memulai Module NoxXB - PRO
 sleep 0.2 && Detected_Game
