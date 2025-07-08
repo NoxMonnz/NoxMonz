3fcf559c9c77a76b9618562c740b6166a8b992c3
NoxMonnz <monnzhexz@gmail.com>
2025-07-08 20:11:45 +0700

    Create design.sh

---
+++ design.sh
@@ -0,0 +1,20 @@
+echo_design() {
+echo " **************************** "
+echo " Developers  = @MonnzHEXZ "
+echo " Credits     = @ReiiEja "
+echo " Version     = V1.4.5 PRO "
+echo " Module Name = NoxXB - PRO "
+echo ""
+}
+
+
+
+echo_design1() {
+echo " "
+echo " █▄░█ ▄▀▀▄ █░█ █░█ █▀▄ ░░ █▀█ █▀▄ ▄▀▀▄"
+echo " █░▀█ █░░█ ▄▀▄ ▄▀▄ █▀█ ▀▀ █▀▀ █▀▄ █░░█"
+echo " ▀░░▀ ░▀▀░ ▀░▀ ▀░▀ ▀▀░ ░░ ▀░░ ▀░▀ ░▀▀░"
+echo "  Ｐｏｗｅｒｅｄ Ｂｙ ＮｏｘＶｅｘ "
+echo " "
+sleep 2
+}
