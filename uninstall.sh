5cb4ecad28960e5148fc55fa028b1b63f93c6b2a
NoxMonnz <monnzhexz@gmail.com>
2025-07-08 16:11:29 +0700

    uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,45 @@
+#!/system/bin/sh
+
+githubFiles3="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/function-deleted.sh"
+
+# Memanggil direktori function.sh dan design.sh
+curl -sL "$githubFiles3" | bash
+
+# Memulai Menghapus Module NoxXB - PRO
+sleep 0.2 && echo_design1
+sleep 0.2 && echo_design
+sleep 2
+echo " Removed Shell Script..."
+sleep 2
+sleep 0.2 && system_read_removed_function
+sleep 0.2 && Removed_mode_vexz
+sleep 0.2 && removed_cachecleaner
+sleep 0.2 && battery
+echo " Loading Process To Removed"
+sleep 0.2 && network_default
+sleep 0.2 && removed_optimize
+sleep 0.2 && disable_render_optimize
+sleep 0.2 && removed_game_driver
+sleep 4
+echo " Loading Final Shell To Removed"
+sleep 0.2 && GarbageCleaner
+sleep 0.2 && Trash
+sleep 0.2 && removed_soc_optimize
+sleep 0.2 && enable_gms
+echo " Success Removed "
+echo ""
+sleep 4
+
+Removed="$1"
+
+case "$Removed" in
+  -Half)
+    # Not Found
+    exit 1
+    ;;
+  -All)
+    sleep 0.2 && Reboot
+    ;;
+esac    
+ 
+  
