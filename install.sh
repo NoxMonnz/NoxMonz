1033de5ed3ff84bc251df5bc19b4d8ca6ca17e69
NoxMonnz <monnzhexz@gmail.com>
2025-07-08 20:46:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/system/bin/sh
 
+GITHUB_FUNCTIONS_URL3="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/design.sh"
+LOCAL_FUNCTIONS_FILE3="/data/local/tmp/design.sh"
 GITHUB_FUNCTIONS_URL="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/function-library.sh"
 LOCAL_FUNCTIONS_FILE="/data/local/tmp/function-library.sh"
 GITHUB_FUNCTIONS_URL2="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/function-library2.sh"
 LOCAL_FUNCTIONS_FILE2="/data/local/tmp/function-library2.sh"
-GITHUB_FUNCTIONS_URL3="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/design.sh"
-LOCAL_FUNCTIONS_FILE3="/data/local/tmp/design.sh"
 
 # Unduh file fungsi ke lokasi sementara
+curl -sL "$GITHUB_FUNCTIONS_URL3" -o "$LOCAL_FUNCTIONS_FILE3"
 curl -sL "$GITHUB_FUNCTIONS_URL" -o "$LOCAL_FUNCTIONS_FILE"
 curl -sL "$GITHUB_FUNCTIONS_URL2" -o "$LOCAL_FUNCTIONS_FILE2"
-curl -sL "$GITHUB_FUNCTIONS_URL3" -o "$LOCAL_FUNCTIONS_FILE3"
 
 # Memulai fungsi file
 source "/data/local/tmp/function-library.sh"
