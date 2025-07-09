8f3f27c550e5b3b51d5433e2c7baf5604ac74ca6
NoxMonnz <monnzhexz@gmail.com>
2025-07-09 21:44:17 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,24 +3,19 @@
 # Argument link GitHub
 GITHUB_FUNCTIONS_URL3="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/design.sh"
 LOCAL_FUNCTIONS_FILE3="/data/local/tmp/design.sh"
-GITHUB_FUNCTIONS_URL2="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/function-library.sh"
-LOCAL_FUNCTIONS_FILE2="/data/local/tmp/function-library.sh"
 GITHUB_FUNCTIONS_URL="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/function-deleted.sh"
 LOCAL_FUNCTIONS_FILE="/data/local/tmp/function-deleted.sh"
 
 # Unduh file fungsi ke lokasi sementara
 curl -sL "$GITHUB_FUNCTIONS_URL3" -o "$LOCAL_FUNCTIONS_FILE3"
 curl -sL "$GITHUB_FUNCTIONS_URL" -o "$LOCAL_FUNCTIONS_FILE"
-curl -sL "$GITHUB_FUNCTIONS_URL2" -o "$LOCAL_FUNCTIONS_FILE2"
 
 # Verifikasi direktori dan file
 chmod +x /data/local/tmp/design.sh
 chmod +x /data/local/tmp/function-deleted.sh
-chmod +x /data/local/tmp/function-library.sh
 
 # Memulai fungsi file
 source "/data/local/tmp/function-deleted.sh"
-source "/data/local/tmp/function-library.sh"
 source "/data/local/tmp/design.sh"
 
 sleep 0.2 && Detected_Game
