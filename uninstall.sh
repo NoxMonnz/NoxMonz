8345d29c4f063a1b646703e5e8016e40164ee3dd
NoxMonnz <monnzhexz@gmail.com>
2025-07-10 22:37:43 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,20 +1,30 @@
 #!/system/bin/sh
 
+# Mulai ulang file
+rm -f /data/local/tmp/design.sh
+rm -f /data/local/tmp/function-library.sh
+rm -f /data/local/tmp/function-deleted.sh
+
 # Argument link GitHub
 GITHUB_FUNCTIONS_URL3="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/design.sh"
 LOCAL_FUNCTIONS_FILE3="/data/local/tmp/design.sh"
+GITHUB_FUNCTIONS_URL2="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/function-library.sh"
+LOCAL_FUNCTIONS_FILE2="/data/local/tmp/function-library.sh"
 GITHUB_FUNCTIONS_URL="https://raw.githubusercontent.com/NoxMonnz/NoxMonz/main/function-deleted.sh"
 LOCAL_FUNCTIONS_FILE="/data/local/tmp/function-deleted.sh"
 
 # Unduh file fungsi ke lokasi sementara
 curl -sL "$GITHUB_FUNCTIONS_URL3" -o "$LOCAL_FUNCTIONS_FILE3"
+curl -sL "$GITHUB_FUNCTIONS_URL2" -o "$LOCAL_FUNCTIONS_FILE2"
 curl -sL "$GITHUB_FUNCTIONS_URL" -o "$LOCAL_FUNCTIONS_FILE"
 
 # Verifikasi direktori dan file
 chmod +x /data/local/tmp/design.sh
+chmod +x /data/local/tmp/function-library.sh
 chmod +x /data/local/tmp/function-deleted.sh
 
 # Memulai fungsi file
+source "/data/local/tmp/function-library.sh"
 source "/data/local/tmp/function-deleted.sh"
 source "/data/local/tmp/design.sh"
 
