de754213a25b64d4c9faaac1ba72d054e6a3c58d
NoxMonnz <monnzhexz@gmail.com>
2025-07-12 11:10:51 +0700

    Update design.sh

--- design.sh
+++ design.sh
@@ -1,9 +1,9 @@
 echo_design() {
-echo " **************************** "
-echo " Developers  = @MonnzHEXZ "
-echo " Credits     = @ReiiEja "
-echo " Version     = V1.4.5 PRO "
-echo " Module Name = NoxXB - PRO "
+echo " ********************************** "
+echo " Developers   =  @MonnzHEXZ "
+echo " Credits      =  @ReiiEja "
+echo " Version      =  V1.4.5 PRO "
+echo " Module Name  =  NoxXB - PRO "
 echo ""
 }
 
